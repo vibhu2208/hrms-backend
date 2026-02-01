c4aa850adaafc301da30fe25de2da2f1a8e8ba99
krishna <krishnaupadhyay207@gmail.com>
2026-02-02 04:08:34 +0530

    doc-s3 config

--- src/routes/publicDocumentUploadRoutes.js
+++ src/routes/publicDocumentUploadRoutes.js
@@ -5,7 +5,7 @@
 
 const express = require('express');
 const router = express.Router();
-const upload = require('../config/multerConfig');
+const upload = require('../config/multerMemoryConfig');
 const {
   validateToken,
   uploadDocument,
